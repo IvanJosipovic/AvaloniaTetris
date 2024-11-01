1b7a1e7b59ed46f4f42c7801c2016384c0ffc783
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2024-10-31 21:43:58 -0700

    wip

--- src/AvaloniaTetris.Browser/Program.cs
+++ src/AvaloniaTetris.Browser/Program.cs
@@ -6,11 +6,9 @@ using Avalonia.Browser;
 
 using AvaloniaTetris;
 
-[assembly: SupportedOSPlatform("browser")]
-
 internal partial class Program
 {
-    private static async Task Main(string[] args) => await BuildAvaloniaApp()
+    private static Task Main(string[] args) => BuildAvaloniaApp()
             .WithInterFont()
             .StartBrowserAppAsync("out");
 
